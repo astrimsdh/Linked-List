3901287706b05d0fb54e096e67194db558b0d78c
Astri Musidah <astry.musidah@gmail.com>
2020-07-06 21:52:24 +0700

    Menambah file latihn2.cpp, kemudian mengcompile menjadi main.exe

--- Latihan1.cpp
+++ Latihan1.cpp
@@ -3,7 +3,14 @@ using namespace std;
 
 int main(){
 	int var = 50;
+	int angka = 1;
+	int *ptr = &var;
 
 	cout<<&var<<" memiliki nilai "<<var<<endl;
+	cout<<"ptr = "<<ptr<<endl;
+
+	ptr = &angka;
+	cout<<"ptr = "<<ptr<<endl;
+	cout<<&angka<<" memiliki nilai "<<angka<<endl;
 	return 0;
 }
