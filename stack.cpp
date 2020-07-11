ae6dc04241ab594a4fee7dbcbfe9ae9729f1fd39
Astri Musidah <astry.musidah@gmail.com>
2020-07-11 23:11:48 +0700

    Menambahkan push data dan pop data pada stack.cpp sebagai implementasi array pada linked list

--- stack.cpp
+++ stack.cpp
@@ -2,11 +2,11 @@
 
 using namespace std;
 
-class Node(){
+class Node{
 public:
 	string url;
 	Node* next;
-}
+};
 
 Node* top;
 
@@ -52,9 +52,61 @@ void cetakIsiStack(){
 	}
 }
 
+int pushNode(string url_new){
+	Node *node_new = new Node();
+	node_new->url = url_new;
 
+	if (isKosong() == true)
+	{
+		node_new->next = NULL;
+	}else{
+		node_new->next = top;
+	}
 
+	top = node_new;
+	node_new = NULL;
+
+	return 0;
+}
+
+int popNode(){
+	if (isKosong() == false)
+	{
+		Node* hapus = top;
+
+		if (hitungNode() == 1)
+		{
+			top == NULL;
+			free(hapus);
+		}else{
+			top = top->next;
+		}
+
+		hapus->next = NULL;
+		free(hapus);
+	}else{
+		cout<<"Stack Kosong"<<endl;
+	}
+
+	return 0;
+}
 
 int main(){
-	
+	top = new Node();
+
+	cout<<"mulai \n";
+	buatKosong();
+	cetakIsiStack();
+
+	cout<<"Masukkan 3 URL histori browser \n";
+	pushNode("http://latihan.nufi3.com");
+	pushNode("http://astrimsdh.github.io");
+	pushNode("http://www.stmik-tasikmalaya.ac.id");
+	cetakIsiStack();
+
+	cout<<"Hapus 1 Node paling atas \n";
+	popNode();
+	cetakIsiStack();
+
+	return 0;
 }
